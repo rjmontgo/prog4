bf31a47d0263453314ef826ff18b64f2e905b8ec
Robert Montgomery <robm4958@gmail.com>
2018-11-18 20:10:41 -0500

    Flipped Textures back so they appear right side up

--- rasterize.js
+++ rasterize.js
@@ -345,7 +345,7 @@ function loadModels() {
                     texCoordToAdd = inputTriangles[whichSet].uvs[whichSetVert]; // get uvs to add
                     inputTriangles[whichSet].glVertices.push(vtxToAdd[0],vtxToAdd[1],vtxToAdd[2]); // put coords in set coord list
                     inputTriangles[whichSet].glNormals.push(normToAdd[0],normToAdd[1],normToAdd[2]); // put normal in set coord list
-                    inputTriangles[whichSet].gltextureCoords.push(texCoordToAdd[0], texCoordToAdd[1]);
+                    inputTriangles[whichSet].gltextureCoords.push(texCoordToAdd[0], 1 - texCoordToAdd[1]);
                     vec3.max(maxCorner,maxCorner,vtxToAdd); // update world bounding box corner maxima
                     vec3.min(minCorner,minCorner,vtxToAdd); // update world bounding box corner minima
                     vec3.add(inputTriangles[whichSet].center,inputTriangles[whichSet].center,vtxToAdd); // add to ctr sum
